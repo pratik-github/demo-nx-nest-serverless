d26b8791f17b269c0a8a9faa3f8590dbb3786851
Siddharth Kanjaria <siddharth.kanjaria@kevit.io>
2024-05-02 13:12:38 +0530

    route and hot reload issue

--- apps/graphql/webpack.config.js
+++ apps/graphql/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
       assets: ['./src/assets'],
       optimization: false,
       outputHashing: 'none',
-      generatePackageJson: true,
+      generatePackageJson: false,
       sourceMap: true
     }),
   ],
